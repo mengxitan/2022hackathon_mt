d8309efd25f840c79a4be408adfff119a1ec2b93
Mengxi Tan <mengxi.tan@pocketgems.com>
2022-04-21 14:23:13 -0700

    Add ship movement

---
+++ 2022 HackathonSailing/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform ship;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - ship.position; //keep camera to be at the same distance from the ship
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ship.position + offset;
+    }
+}
---
+++ 2022 HackathonSailing/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour {
+
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput;
+    public float horitzontalMultiplier = 2;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime; 
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horitzontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+    }
+}
