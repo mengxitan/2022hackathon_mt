502edb669ba185e7d92bf9a82c5bdf0c1fc7ea8f
Mengxi Tan <mengxi.tan@pocketgems.com>
2022-04-22 11:55:27 -0700

    Change rivertile behavior

--- 2022 HackathonSailing/Assets/Scripts/RiverSpawner.cs
+++ 2022 HackathonSailing/Assets/Scripts/RiverSpawner.cs
@@ -20,7 +20,7 @@ public class RiverSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 15; i++){
+        for (int i = 0; i < 20; i++){
 
             SpawnTile();
 
